5405b0b7dc684c5d27c39880cc28e0294b339669
Tang <tanghaha1018@gmail.com>
2016-05-18 22:00:41 +0800

    Initial Commit

---
+++ NTU-intro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NTU-intro
+//
+//  Created by Tang on 2016/5/18.
+//  Copyright © 2016年 Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
